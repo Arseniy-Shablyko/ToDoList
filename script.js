e44bacbc2d34d325b380e6e028894754b9e616d6
Arseniy-Shablyko <241208arseniy@gmail.com>
2026-03-03 18:41:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let add_b = document.getElementById("add");
 let idCounter = 1;
+let list = document.getElementById("list");
+let activeArrayIndex = 0;
+let activeArray = [];
+let doneArrayIndex = 0;
+let doneArray = [];
 
 add_b.addEventListener('click', function(){
-    let list = document.getElementById("list");
     let newItem = document.createElement("li");
     newItem.id = idCounter;
     idCounter++;
     newItem.textContent = prompt("Input text...");
     list.appendChild(newItem);
+    activeArray[activeArrayIndex] = newItem.textContent;
+    activeArrayIndex++;
+});
+
+list.addEventListener('click', function(event){
+    if(event.target.tagName === 'LI'){
+        let text = event.target.innerText;
+        doneArray[doneArrayIndex] = text;
+        doneArrayIndex++;
+        activeArray = activeArray.filter(item => item !== text);
+        activeArrayIndex--;
+        event.target.remove();
+    }
 });
